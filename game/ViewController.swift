0244929227b180b341a00ae7c6aa6684b2c6833c
Mariana BORTNICHUK <mbortnic@e1r8p18.unit.ua>
2018-08-12 18:23:44 +0300

    initial commit

---
+++ game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by Mariana BORTNICHUK on 8/12/18.
+//  Copyright © 2018 Mariana BORTNICHUK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
