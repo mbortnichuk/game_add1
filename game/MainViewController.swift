67d4b6872d0fa93abcd468a203f636a77b724f99
Mariana BORTNICHUK <mbortnic@e1r7p18.unit.ua>
2018-08-14 14:22:29 +0300

    a few major changes

--- game/MainViewController.swift
+++ game/MainViewController.swift
@@ -10,26 +10,63 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var numbersLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var score:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+        
+        inputField?.addTarget(self, action: #selector(textFieldDidChange(textField:)), for:UIControlEvents.editingChanged)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateScoreLabel() {
+        scoreLabel?.text = "\(score)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setRandomNumberLabel() {
+        numbersLabel?.text = generateRandomString()
     }
-    */
+    
+    @objc func textFieldDidChange(textField:UITextField) {
+        if inputField?.text?.characters.count ?? 0 < 4 {
+            return
+        }
+        if let numbers_text = numbersLabel?.text,
+            let input_text = inputField?.text,
+            let numbers = Int(numbers_text),
+            let input = Int(input_text) {
+            print("Comparing: \(input_text) minus \(numbers_text) == \(input - numbers)")
+            
+            if (input - numbers == 1111) {
+                print("Correct!")
+                score += 1
+            } else {
+                print("Incorrect!")
+                score -= 1
+            }
+        }
+        setRandomNumberLabel()
+        updateScoreLabel()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func generateRandomString() -> String {
+        var result:String = ""
+        
+        for _ in 1...4 {
+            var digit:Int = Int(arc4random_uniform(8) + 1)
+            result += "\(digit)"
+        }
+        return result
+    }
 
 }
